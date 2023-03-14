2826426bf07bcba2aceabfd9ce4bdde8c3607cc5
berryjoey52 <62221886+berryjoey52@users.noreply.github.com>
2023-03-14 09:36:58 -0400

    updated float and integer values to convert to string values in line of the print statement.

--- salary_calculator.py
+++ salary_calculator.py
@@ -4,39 +4,33 @@ weeks_in_year = 52.1429
 def salary():
     hours = float(input("How many hours will you be working a week? (ex. 40) "))
     rate = float(input("What is your hour rate in $ (ex. 17.75)"))
-    salary = str((hours * rate) * weeks_in_year)
-    hours = str(hours)
-    rate = str(rate)
-    print("Salary: $" + salary)
-    print("Weekly Hours: " + hours)
-    print("Rate: $" + rate + "/hr")
+    salary = ((hours * rate) * weeks_in_year)
+    
+    print(f"Salary: ${salary:.2f}")
+    print(f"Weekly Hours:{hours}")
+    print(f"Rate: ${rate:.2f}/hr")
     menu()
-    #print("Your yearly salry will be $" + salary)
 
 def hours():
     rate = float(input("What is your hour rate in $? (ex. 17.75)"))
     salary = float(input("What is your targeted salary? (ex. 60000)"))
-    hours = str(((salary / weeks_in_year) / rate))
-    rate = str(rate)
-    salary = str(salary)
-    print("Salary: $" + salary)
-    print("Weekly Hours: " + hours)
-    print("Rate: $" + rate + "/hr")
+    hours = (((salary / weeks_in_year) / rate))
+    
+    print(f"Salary: ${salary:.2f}")
+    print(f"Weekly Hours:{hours}")
+    print(f"Rate: ${rate:.2f}/hr")
     menu()
-    #print("You will need to work " + hours + " hours a week at $" + rate + " to make $" + salary)
 
 def rate():
     salary = float(input("What is your targeted salary? (ex. 60000)"))
     hours = float(input("How many hours will you be working a week? (ex. 40) "))
-    rate = str((salary / hours) / weeks_in_year) 
-    hours = str(hours)
-    salary = str(salary)
-    print("Salary: $" + salary)
-    print("Weekly Hours: " + hours)
-    print("Rate: $" + rate + "/hr")
+    rate = ((salary / hours) / weeks_in_year) 
+   
+    print(f"Salary: ${salary:.2f}")
+    print(f"Weekly Hours:{hours}")
+    print(f"Rate: ${rate:2f}/hr")
     menu()
-    #print("Your rate will be $" + rate + "at " + hours + " hours a week in order to make $" + salary)
-
+   
 def help():
     print("Selection 1 will let you calcuate the yearly salary if you already have the hourly wage and numbers of hours worked. \nSelection 2 will give you the hours needed to work per week if you have the salary and rate per hour. \nSelection 3 Will give you the hourly rate if you have the Salary and the hours worked each week. \nSelection 0 will have you exit the program.")
 
